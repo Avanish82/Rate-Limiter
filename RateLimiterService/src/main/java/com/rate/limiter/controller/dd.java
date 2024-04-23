067c55416b50af32cd3d023f85efd50a0997becc
AvanishKumarSingh1 <avanish.as@gmail.com>
2024-04-23 23:12:10 +0530

    first
    commit

---
+++ RateLimiterService/src/main/java/com/rate/limiter/controller/dd.java
@@ -0,0 +1,14 @@
+package com.rate.limiter.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class dd {
+
+	private int id;
+	private String name;
+}
---
+++ RateLimiterService/src/main/java/com/rate/limiter/service/RateLimiterService.java
@@ -0,0 +1,13 @@
+//package com.rate.limiter.service;
+//
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class RateLimiterService {
+//
+//    public ResponseEntity<String> invokeRateLimiterTest() {
+//        return ResponseEntity.ok("Rate Limiter Test");
+//    }
+//
+//}
