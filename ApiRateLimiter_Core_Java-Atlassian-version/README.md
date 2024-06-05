605cfa4e3e495f512b7e124f462f5571a2777ea7
AvanishKumarSingh1 <avanish.as@gmail.com>
2024-06-06 01:15:47 +0530

    first commit
    user based ratelimit

---
+++ ApiRateLimiter_Core_Java-Atlassian-version/README.md
@@ -0,0 +1,9 @@
+# ApiRateLimiter_Core_Java
+Thread safe implementaion of ratelimter using core java and singleton design pattern
+
+
+Atlassian Problem Statement:
+
+
+Problem Title: Rate Limiter
+Video Soln : https://youtu.be/5GNvyh0Ewm8
---
+++ ApiRateLimiter_Core_Java-Atlassian-version/src/main/java/constants/RateLimiterConstant.java
@@ -0,0 +1,29 @@
+package constants;
+
+public class RateLimiterConstant {
+	
+    public static final int BUCKET_SIZE = 5;
+    public static final long TIME_WINDOW = 1000;   //1 seconds (system always taks mile seconds)
+    
+//    <javafx.maven.plugin.version>0.0.8</javafx.maven.plugin.version>
+//    
+//    <dependency>
+//    <groupId>org.openjfx</groupId>
+//    <artifactId>javafx-controls</artifactId>
+//    <version>11</version>
+//  </dependency>
+//</dependencies>
+//  
+//  <build>
+//    <plugins>
+//    <plugin>
+//        <groupId>org.openjfx</groupId>
+//        <artifactId>javafx-maven-plugin</artifactId>
+//        <version>0.0.1</version>
+//        <configuration>
+//            <mainClass>HelloFX</mainClass>
+//        </configuration>
+//    </plugin>
+//</plugins>
+//  </build>
+}
